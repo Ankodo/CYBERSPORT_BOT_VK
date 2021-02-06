d6208a815b79b293332122ed05f5c50e8ad914f5
Alexander Shashkov <ruthheim@hotmail.com>
2021-02-06 16:06:45 +0300

    ident

--- identification.py
+++ identification.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 # Токен группы для callback
-token = "40cea5ae46ce1b88ea9e5978f45136522e553292f0c1a8dd1cede663e6a61d96e7bae365eb0e04ffad28b"
+token = "a3f417d3ff86776d39d5ec5944f957cfe8621cb6e3dc7876565f9857028e4ca9ca193f97a40de6ef28414"
 # Id группы
-id = "202117434"
+id = "199323686"
 # Ссылка с токеном для приложения рассылки (достаточно поставить свой токен после token=)
-miniapps_token = "https://broadcast.vkforms.ru/api/v2/broadcast?token=api_83419_NmYxYBVTFOhNd89ZkrzZFNGu"
+miniapps_token = "https://broadcast.vkforms.ru/api/v2/broadcast?token=api_83359_SgPRwkgwgnMLcCqPyVeTxd4n"
