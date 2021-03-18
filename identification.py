77096fb1b2f1eee868d0abb579daac57abdd3696
Ankodo <andro9905@gmail.com>
2021-03-18 12:55:50 +0300

    Update identification.py

--- identification.py
+++ identification.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 # Токен группы для callback
-token = "a3f417d3ff86776d39d5ec5944f957cfe8621cb6e3dc7876565f9857028e4ca9ca193f97a40de6ef28414"
+token = "f4d1dd31b59bb778126ffcf323e8d1980ae7526471430e244d94e2c3a1baf6f91f52055d3e4addda3f960"
 # Id группы
-id = "199323686"
+id = "27055949"
 # Ссылка с токеном для приложения рассылки (достаточно поставить свой токен после token=)
-miniapps_token = "https://broadcast.vkforms.ru/api/v2/broadcast?token=api_83359_SgPRwkgwgnMLcCqPyVeTxd4n"
+miniapps_token = "https://broadcast.vkforms.ru/api/v2/broadcast?token=api_83494_vAkx0gQxxCMc9Xxi8gxdi5fg"
 # Код подтверждения для callback
-confirmation = "abcde"
+confirmation = "8309c96c"
 # Адрес, по которой будут слушаться запросы
 link = "/callback"
